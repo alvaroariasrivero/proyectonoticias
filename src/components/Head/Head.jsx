ae364f29d96e6b1a0342c7a10654d686cca6377a
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2021-12-15 16:11:43 +0100

    login aparece en header por context

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -1,11 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Nav from "../Nav/Nav";
+import {userContext} from '../../context/userContext';
 
 class Head extends Component {
   render() {
     return (
     <header>
       <Nav/>
+      <userContext.Consumer>
+        {
+          ({user, logout}) => user ?
+          <Fragment>
+              <h3>Hola {user}</h3>
+              <button onClick={logout}>Logout</button>
+          </Fragment>
+          : ('')
+        }
+      </userContext.Consumer>
     </header>)
   }
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
+import {userContext} from '../../context/userContext';
 
 class Home extends Component {
+  static contextType = userContext
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       user:''
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const user = event.target.elements.user.value;
+    console.log('Esto es user', user);
+    this.setState({user});
+    const{login} = this.context;
+    login(user)
+  }
+  
   render() {
     return (
-    <form>
-      <label htmlFor='text'>Nombre: </label>
-      <input type="text" name='name'/>
+    <form onSubmit={this.handleSubmit}>
+      <label htmlFor='user'>Nombre: </label>
+      <input type="text" name='user'/>
       <input type="submit" />
     </form>);
   }
