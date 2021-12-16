0a23462e8ff037b2598f1b228595c07249454fee
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2021-12-16 13:23:15 +0100

    redirect hecho

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
+import { Navigate } from 'react-router-dom'
 
 class Form extends Component {
-  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      redirect: null
+    }
+  }  
   handleSubmit = (event) => {
     event.preventDefault();
     const title = event.target.elements.title.value;
     const category = event.target.elements.category.value;
     const content = event.target.elements.content.value;
     const picture = event.target.elements.picture.value;
-    console.log("Estos son los datos del form:", title, category, content, picture)
-    this.props.createNew(title, category, content, picture)
+    // console.log("Estos son los datos del form:", title, category, content, picture)
+    this.props.createNew(title, category, content, picture);
+    this.setState({ redirect: true });
   }
   render() {
+    
+    const { redirect } = this.state;
+    
+    if (redirect) {
+      return <Navigate to='/list'/>;
+    }
+    
     return <div>
             <form onSubmit={this.handleSubmit}>
                 <label htmlFor="title">Title: </label>
